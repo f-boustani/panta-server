bda3ae7c96e34ca3bdc82571532f4612c2261304
Fariba Boustani <fariba.boustani@gmail.com>
2015-01-24 21:02:30 +0330

    test notif

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from gcm import *
 message=' it works :D'
 gcm = GCM("AIzaSyCWZBvIjLg0kmBELKsObqostZHx2AZWCvQ")
 data = {'the_message': message}
-reg_id = "APA91bFgFwRa271nk1i9wcFFEcHj8QowcLA1KytPLTZFhBFyHRAnkGjl9DuuZagXj4ODPTsx22ZcbVWt_JaXJH20-MoH-Uk7Ud62Z6-LB69u_t4O_kJaD8bz6qFS7BpXkn4ykmtJoZCC9Wlj4uT-MVYQjPBu6m89mw"
+reg_id = "APA91bHIyefim8zLFBaWIR6T8QqwIGVLMyOCTonMtl7egKs3L8XXI9z4hsBoHDoyGQV-E2LFeAE2KFZBua3_6NgOfiTwgSFtVeNRpAhaolK4gjr3ZRdZuh2W3g4kk29PKXAPj_moHT6GJKbu5WBJHv2dM2wcJO45oA"
 gcm.plaintext_request(registration_id=reg_id, data=data)
 
 a=gcm.plaintext_request(registration_id=reg_id, data=data)
