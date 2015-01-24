61798e75a0d995261b9ee1cd00ac86a5e14509ef
Fariba Boustani <fariba.boustani@gmail.com>
2015-01-24 21:23:26 +0330

    !!!!!!!!!!!!

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from gcm import *
 message=' it works :D'
 gcm = GCM("AIzaSyCWZBvIjLg0kmBELKsObqostZHx2AZWCvQ")
 data = {'the_message': message}
-reg_id = "APA91bFgFwRa271nk1i9wcFFEcHj8QowcLA1KytPLTZFhBFyHRAnkGjl9DuuZagXj4ODPTsx22ZcbVWt_JaXJH20-MoH-Uk7Ud62Z6-LB69u_t4O_kJaD8bz6qFS7BpXkn4ykmtJoZCC9Wlj4uT-MVYQjPBu6m89mw"
+reg_id = "godfather@y.com | APA91bGvcB28g_CATZVAsgrSod52jQk4z5wz8c5EyiyrZZH6RrG21hXdXCWAH8q5140cYROPcjUUxEU6MXxhs9G511rHx6bCF0VMj66zy176Fb14Fwdu-ojkHIUmGifFKBoeakZ-jZxKdKB-Td5DZDjLqqtZUm3oQQ"
 gcm.plaintext_request(registration_id=reg_id, data=data)
 
 a=gcm.plaintext_request(registration_id=reg_id, data=data)
