5d70700075034c33d3a4c897e5178663d28b2eb2
Fariba Boustani <fariba.boustani@gmail.com>
2015-01-26 00:59:42 +0330

    ...

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from gcm import *
 message=' it works :D'
 gcm = GCM("AIzaSyBJ2eSyVNiT9Xfh-KsvmjjSvoY_rs7VvSA")
 data = {'the_message': message}
-reg_id = "APA91bG5kogBn7hWgFzOk9wsTEKDIP_t2SVql4Zie9oICAUe5lGihlBhpCjexfLAayz9HHJnENGNdmN5SL9hX4LP35gUVDR3QUF_IoPH_HNzJ0kwOCTKA3l-7Tl9PHd7CqMvDrbOvL0NmgLfzkiui8vjsL24t4KFvg"
+reg_id = "APA91bHzei_ZWbffypm6Y24nY0b8xK_i9DUfXaBUZqTJ6ekVBiFw8KSONv-RUnvpBfGYIe0wh6rTHsxhEZpwKKRuaaAZyqOzIjco7Q90QGdLX3eQp4RoVX7tdXybOqTvwtb9H63ahmrjbFMooVf4MNPxuKcZqIkONA"
 
 try:
     canonical_id = gcm.plaintext_request(registration_id=reg_id, data=data)
