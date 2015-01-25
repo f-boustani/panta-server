2906acfff989ba57434a3b9f0b5a9fb0c2e63cd3
Fariba Boustani <fariba.boustani@gmail.com>
2015-01-26 01:33:51 +0330

    ..

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ class NotRegistered(Exception):
 message=' it works :D'
 gcm = GCM("AIzaSyBJ2eSyVNiT9Xfh-KsvmjjSvoY_rs7VvSA")
 data = {'the_message': message}
-reg_id = " APA91bFgFwRa271nk1i9wcFFEcHj8QowcLA1KytPLTZFhBFyHRAnkGjl9DuuZagXj4ODPTsx22ZcbVWt_JaXJH20-MoH-Uk7Ud62Z6-LB69u_t4O_kJaD8bz6qFS7BpXkn4ykmtJoZCC9Wlj4uT-MVYQjPBu6m89mw"
+reg_id = "APA91bFgFwRa271nk1i9wcFFEcHj8QowcLA1KytPLTZFhBFyHRAnkGjl9DuuZagXj4ODPTsx22ZcbVWt_JaXJH20-MoH-Uk7Ud62Z6-LB69u_t4O_kJaD8bz6qFS7BpXkn4ykmtJoZCC9Wlj4uT-MVYQjPBu6m89mw"
 
 try:
     canonical_id = gcm.plaintext_request(registration_id=reg_id, data=data)
