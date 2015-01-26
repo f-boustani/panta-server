c2200d25f858983b055cec08450baf5632d4aee5
Fariba Boustani <fariba.boustani@gmail.com>
2015-01-26 10:53:49 +0330

    checktime url (append / )

--- check_time.py
+++ check_time.py
@@ -9,14 +9,14 @@ import urllib2
 
 def task_deadline():
 
-	urllib2.urlopen('http://127.0.0.1:8800/check_deadline')
+	urllib2.urlopen('http://127.0.0.1:8800/check_deadline/')
 	t = threading.Timer(300.0, task_deadline)
 	t.start()  
 	
 #********************************************************************
 def project_deadline():
 
-	urllib2.urlopen('http://127.0.0.1:8800/check_end_project')
+	urllib2.urlopen('http://127.0.0.1:8800/check_end_project/')
 	
 	p = threading.Timer(86400.0, project_deadline)
 	p.start()  
