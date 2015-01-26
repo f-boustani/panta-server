92d9ec7f9da1fa61aed857d8bd990039c0ae7481
Fariba Boustani <fariba.boustani@gmail.com>
2015-01-26 12:32:10 +0330

    test

--- test.py
+++ test.py
@@ -1,22 +1,9 @@
 from gcm import *
-class InvalidRegistration(Exception):
-	pass
-
-class MismatchSenderId(Exception):
-	pass
-	
-class Unavailable(Exception):
-	pass
-
-class NotRegistered(Exception):
-	pass
-		
-
 
 message=' it works :D'
 gcm = GCM("AIzaSyBJ2eSyVNiT9Xfh-KsvmjjSvoY_rs7VvSA")
 data = {'the_message': message}
-reg_id = "APA91bF6SCQRJ_d8g7-A6o5c9PZsBtOo0TYxPgejM_4P3ByZJyJd2KOEvZTYo0l7BWoO8BBp4lPR-1hNRo6SHZ_M4hAg5jaya3ja5IrU-01uIuslD40X_ZcWvUSLyXElXckCCT2mw-Zvrs-goH2273A6l4dQlLdOHQ"
+reg_id = "APA91bFIhg5dDv9vnJUgslIW3GUky0Sx4SNQI0_qIzn7ZKu9DAN11m73aKC-qJ72ju54FyDAt7OMcrafsxXIs-ADP6KjEj2BCVL_A7s9bo222Cwehmm_ViSii9HPuDzzAtjrNLp63wbqxq4VL8OdbocKD6mQ75bVQg"
 
 try:
     canonical_id = gcm.plaintext_request(registration_id=reg_id, data=data)
@@ -25,17 +12,9 @@ try:
     	print 'reg_id change,must replace'
         
        
-except InvalidRegistration:
-    print 'Invalid Reg_id'
+except:
+    print 'except'
    
-except NotRegistered:
-    print not registered
-
-except Unavailable :
-    print 'Unavailable'
-
-except MismatchSenderId :
-    print 'Unavailable'
 
 
 
